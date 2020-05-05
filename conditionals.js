9227220196f88d3aac07c01f1953ba5905e269b1
Graham Mothersill <graham.mothersill@gmail.com>
2020-05-05 18:20:25 +0000

    Use conditionals to tell people about the weather, and going outside in times of COVID.

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+
+const temperature = -15;
+
+if (temperature < -10) {
+  console.log("Get the parka out!")
+} else if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside! (Please maintain a minimum of 6 feet of distance between yourself and others.)");
+
