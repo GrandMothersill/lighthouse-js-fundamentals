164ce0591f9635bff59792842bf5ae7de4b9dd86
Graham Mothersill <graham.mothersill@gmail.com>
2020-05-05 20:18:22 +0000

    Used while loop to repeat a chorus line

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Into the woods and out of the woods and";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat % 3 === 0 && repeat !== 0) {
+    console.log("*keychange up, think Love on Top*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Home before dark!");
