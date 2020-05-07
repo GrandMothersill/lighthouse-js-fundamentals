0fceea6b98950ad48119956ead0474318df612ca
Graham Mothersill <graham.mothersill@gmail.com>
2020-05-07 01:47:56 +0000

    Tinkered with functions that say hello

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+
+const sayJello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayJello("Prospero");
