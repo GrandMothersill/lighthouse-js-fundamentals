4085410649f01febd8679495a7f9e605b24b7e35
Graham Mothersill <graham.mothersill@gmail.com>
2020-05-07 22:57:34 +0000

    used while and for loops to make banana bread

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let w = 0
+while (w < ingredients.length) {
+  console.log(ingredients[w])
+  w++
+};
+
+for (let f = 0; f < ingredients.length; f++) {
+  console.log(ingredients[f])
+};
+
+
+for (let r = (ingredients.length - 1); r >= 0; r--) {
+  console.log(ingredients[r])
+};
+
+// Write a while loop that prints out the contents of ingredients:
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
